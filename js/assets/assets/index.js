4742dc4054eda55a995bec58b56cc5d17b00b1c6
Yewilsaw <yewilsawc@gmail.com>
2026-01-04 15:50:42 +0300

    Delete js/assets/assets/index.js

--- js/assets/assets/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-exports.hello = functions.https.onRequest((req, res) => {
-  res.send("Hello from Firebase!");
-});
