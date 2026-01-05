54126e40373e84490baa3addd766a7debd90bebc
Yewilsaw <yewilsawc@gmail.com>
2026-01-05 18:24:55 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 
 const firebaseConfig = {
 apiKey: "AIzaSyB9fmgduk-bcSRTPL6FdcKKgqvTtQoICpE",
@@ -19,3 +21,12 @@ export const storage = getStorage(app);
 
 export const signIn = () => signInAnonymously(auth);
 export const signOutUser = () => signOut(auth);
+// Sign up with email + password
+export async function signUp(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+// Sign in with email + password
+export async function signInWithEmail(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
