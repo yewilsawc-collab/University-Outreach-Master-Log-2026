1924c60ab25b2657f0782cef16884b31b7d31a2e
Yewilsaw <yewilsawc@gmail.com>
2026-01-09 21:56:58 +0300

    Create discovery.js

---
+++ public/assets/views/discovery.js
@@ -0,0 +1,7 @@
+console.log("Discovery view loaded");
+
+// Placeholder for future recommendation engine
+document.getElementById("discovery-feed").innerHTML = `
+    <p>Discovery engine coming soon...</p>
+`;
+
