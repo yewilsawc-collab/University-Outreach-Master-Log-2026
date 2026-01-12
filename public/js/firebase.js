1a8cff6975bea869edda8f3f2431f5e959592cc2
Yewilsaw <yewilsawc@gmail.com>
2026-01-12 13:01:54 +0300

    Update and rename firebase.js to public/js/firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,4 @@
-// firebase.js - REFACTORED
+// firebase.js 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
